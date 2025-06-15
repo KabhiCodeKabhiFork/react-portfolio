6d9b651b6f69e3b5712a3fe9fb0e280fbcd77cb7
Kartikey <148078124+KabhiCodeKabhiFork@users.noreply.github.com>
2025-06-15 14:22:55 +0530

    ada

--- react-portfolio/src/content_option.js
+++ react-portfolio/src/content_option.js
@@ -1,5 +1,4 @@
 import image1 from "./assets/images/pexels-pixabay-534216.jpg"
-import image2 from "./assets/images/1032028070862920821.jpg"
 import image3 from "./assets/images/image.png"
 import image4 from "./assets/images/Screenshot 2025-06-14 at 10.58.49 PM.png"
 import image5 from "./assets/images/nba.jpg"
