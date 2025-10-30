8ba15f644db0be024f5077c156e17ba2d806b41a
Kartikey <148078124+KabhiCodeKabhiFork@users.noreply.github.com>
2025-10-30 19:58:26 +0530

    added colors

--- src/components/themetoggle/index.js
+++ src/components/themetoggle/index.js
@@ -3,19 +3,11 @@ import { WiMoonAltWaningCrescent4 } from "react-icons/wi";
 
 
 const Themetoggle = () => {
-  const [theme, settheme] = useState(localStorage.getItem("theme"));
-  const themetoggle = () => {
-    settheme(theme === "dark" ? "light" : "dark");
-  };
   useEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme ); 
-  }, [theme]);
-  return (
-    <div className="nav_ac" onClick={themetoggle}>
-      <WiMoonAltWaningCrescent4 />
-    </div>
-  );
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark' );
+  }, []);
+  return null;
 };
 
 export default Themetoggle;
